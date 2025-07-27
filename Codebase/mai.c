e00b6fb7f2dea40edd70bb95e6fa0b8dc8a81aa3
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-07-27 20:03:43 +0530

    MU

---
+++ Codebase/mai.c
@@ -0,0 +1,15 @@
+/*
+this is basic code of C Language
+\n - It's used for line change
+\t - It's used for containing space
+     between two words
+*/
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    a = 10;
+    b = 20;
+    sum = a + b;
+    printf("Value of sum is: %d",sum);
+    return 0;
+}
