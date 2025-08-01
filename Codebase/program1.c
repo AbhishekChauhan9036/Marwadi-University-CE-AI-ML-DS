cd26cbf6f1e9bda4fe576eb7a8a02840672f11ad
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-08-01 10:55:00 +0530

    MU

--- Codebase/program1.c
+++ Codebase/program1.c
@@ -1,5 +1,13 @@
-#define print printf
-#include<stdio.h>
-void main(){
-    print("Hello\tWorld!");
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int num1, num2, div = 0;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    div = num1 / num2;
+    printf("Quotient of num1 and num2 is: %d", div);
+    return 0;
 }
