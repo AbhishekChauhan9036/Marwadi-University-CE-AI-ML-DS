96ee30316cbeca10e009abd1009de75f532d8a05
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-08-03 09:41:02 +0530

    MU

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    int a, b, sum=0;
-    a = 10;
-    b = 20;
-    sum = a + b;
-    printf("sum of a and b is: %d",sum);
-}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    printf("Hello World...\n");
-    printf("How are you?..\n");
-
-    int a;
-    scanf("%d",&a);
-    printf("%d",a);
-}
