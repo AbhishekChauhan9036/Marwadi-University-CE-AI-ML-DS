258b1edab525e440cfbeb3aac1a3f3d94846d8db
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-08-11 10:38:42 +0530

    MU

---
+++ Codebase/logicanoperator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    --a; // Pre Decrement Operator
+    a--; // Post Decremt Operator
+    a++; // Post Increment Operator
+    ++a; // Pre Incremet Operator
+    printf("%d",a);
+    return 0;
+}
