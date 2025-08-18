8f40be57b5eedca63a190bfb5fde42f858650b8b
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-08-18 13:30:29 +0530

    MU

---
+++ Codebase/twoDMatix.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[n][m];
+    int brr[m][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            brr[i][j] = arr[j][i];
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", brr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
