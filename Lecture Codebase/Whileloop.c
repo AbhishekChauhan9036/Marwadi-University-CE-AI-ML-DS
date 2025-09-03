909ecbff6440060b66a3a0497469a8799cd6ecad
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-09-03 10:39:59 +0530

    MU

--- Lecture Codebase/Whileloop.c
+++ Lecture Codebase/Whileloop.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main(){
-    int i =1;
-    while(i<=10){
-        printf("How are you?\n");
-        i++;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=9;j++){
+            if(j>=6-i && j<=4+i){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
     return 0;
 }
