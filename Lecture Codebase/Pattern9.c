a9358ca485b403a88d07a3be6b7d2d47bc836b32
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-09-07 11:24:57 +0530

    MU

--- Lecture Codebase/Pattern9.c
+++ Lecture Codebase/Pattern9.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-int main(){
-    int k=1;
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            if(j<=i){
-                printf("%d ",5*k);
-                k++;
-            }else{
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 5; i++){
+        int k = 1;
+        for (int j = 1; j <= 9; j++){
+            if (j <= i * 2 - 1){
+                printf("%d", k);
+                i>j?k++:k--;
+            }
+            else{
                 printf(" ");
             }
         }
