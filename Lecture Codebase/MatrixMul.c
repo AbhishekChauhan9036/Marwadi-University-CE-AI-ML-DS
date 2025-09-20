360d1ae7896d41eab91dbd20ca8482c167ecb676
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-09-20 12:51:57 +0530

    MU

--- Lecture Codebase/MatrixMul.c
+++ Lecture Codebase/MatrixMul.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main(){
-    int arr[2][2]={{1,2},{3,4}};
-    int brr[2][2]={{5,6},{0,7}};
-    int mul[2][2]={{0,0},{0,0}};
+    int arr[2][2]= {{1,2},{3,4}};
+    int brr[2][2]= {{5,6},{0,7}};
+    int mul[2][2]= {{0,0},{0,0}};
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
             for(int k=0;k<2;k++){
-                mul[i][j]=mul[i][j]+arr[i][k]*brr[k][j];
+                mul[i][j] = mul[i][j]+arr[i][k]*brr[k][j];
             }
         }
     }
 
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            printf("%3d ",mul[i][j]);
+            printf("%2d ",mul[i][j]);
         }
         printf("\n");
     }
--- Lecture Codebase/Stringss.c
+++ Lecture Codebase/Stringss.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str1[]={"Veron\0"};
-    int i=0,cnt=0;
-    while(str1[i]!='\0'){
-        cnt++;
-        i++;
-    }
-    printf("%d",cnt);
+    char fName[]={"Sri Ram"};
+    printf("%d\n",strlen(fName));
+    printf("%s",fName);
     return 0;
 }
