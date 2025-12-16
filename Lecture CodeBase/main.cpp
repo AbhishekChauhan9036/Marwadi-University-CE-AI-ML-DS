30512eec871a92b8a2a9194ec0cb4133b6ea40ab
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-12-16 10:15:07 +0530

    MU

--- Lecture CodeBase/main.cpp
+++ Lecture CodeBase/main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
+class A{
+    private:
+    int x;
+    public: 
+    A(int a){
+        x=a;
+    }
+    void displayDetails(){
+        cout<<"Value of x: "<<x<<endl;
+    }
+};
 int main(){
     cout<<"Hello World!"<<endl;
+    A obj(10);
+    obj.displayDetails();
     return 0;
 }
